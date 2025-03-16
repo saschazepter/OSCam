66079649c5f0350f61eadc1492374e9e1bded27c
ohmza <3435-ohmza@users.noreply.git.streamboard.tv>
2025-03-16 20:45:27 +0000

    Simplify entitlements_bit.html code
    * Simplify entitlements_bit.html code (introduce more general function that could be reused, get rid of unnecessary function)

--- globals.h
+++ globals.h
@@ -372,7 +372,7 @@
 #define WIKI_URL				"https://wiki.streamboard.tv/wiki"
 #define BOARD_URL				"https://board.streamboard.tv"
 #ifndef CS_VERSION
-#define CS_VERSION				"2.25.03-11872"
+#define CS_VERSION				"2.25.03-11873"
 #endif
 #ifndef CS_GIT_COMMIT
 #define CS_GIT_COMMIT			"a2b4c6d8"
