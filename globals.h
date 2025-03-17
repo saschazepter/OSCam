a32ffba4c5732c8a91a44fb620eb222abd1c092f
ohmza <3435-ohmza@users.noreply.git.streamboard.tv>
2025-03-17 09:26:55 +0000

    Fix "CacheEX AIO Logging" config section always show on webif although CS_CACHEEX_AIO is not selected
    * Fix "CacheEX AIO Logging" config section always show on webif although CS_CACHEEX_AIO is not selected (part 3 of 3)
    
    * Fix "CacheEX AIO Logging" config section always show on webif although CS_CACHEEX_AIO is not selected (part 2 of 3)
    
    * Fix "CacheEX AIO Logging" config section always show on webif although CS_CACHEEX_AIO is not selected (part 1 of 3)

--- globals.h
+++ globals.h
@@ -372,7 +372,7 @@
 #define WIKI_URL				"https://wiki.streamboard.tv/wiki"
 #define BOARD_URL				"https://board.streamboard.tv"
 #ifndef CS_VERSION
-#define CS_VERSION				"2.25.03-11873"
+#define CS_VERSION				"2.25.03-11874"
 #endif
 #ifndef CS_GIT_COMMIT
 #define CS_GIT_COMMIT			"a2b4c6d8"
