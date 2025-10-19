1e508429b2729b39531587fb85992ab5d96e03b4
WXbet <4-WXbet@users.noreply.git.streamboard.tv>
2025-10-19 19:26:31 +0200

    signing: more cleanups

--- oscam-signing.c
+++ oscam-signing.c
@@ -111,9 +111,14 @@ static unsigned int two_digits_to_uint(const char **s) {
 }
 
 static time_t ASN1_TIME_to_posix_time(const ASN1_TIME *t) {
-	if (!t) return -1;
-	const char *s = (const char*)t->data;
-	if (!s) return -1;
+    if (!t) return -1;
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L  // changed in OpenSSL 1.1.0+
+    const unsigned char *us = ASN1_STRING_get0_data((const ASN1_STRING*)t);
+    const char *s = (const char*)us;
+#else
+    const char *s = (const char*)t->data;
+#endif
+    if (!s) return -1;
 
 	unsigned int year, month, day, hour, min, sec;
 	switch(t->type) // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.5.1
@@ -165,218 +170,187 @@ static EVP_PKEY *verify_cert(void)
 	OpenSSL_add_all_algorithms();
 #endif
 
-	if ((pCert = X509_new()))
+	pBio = BIO_new(BIO_s_mem());
+	if (!pBio)
 	{
-		if ((pBio = BIO_new(BIO_s_mem())))
+		cs_log("Error: BIO_new() failed");
+		return NULL;
+	}
+
+	// Load built-in cert from memory into BIO object
+	if (!BIO_puts(pBio, config_cert))
+	{
+		cs_log("Error: BIO_puts() failed");
+		BIO_free(pBio);
+		return NULL;
+	}
+
+	// Read cert in PEM format from BIO (allocates X509 internally)
+	pCert = PEM_read_bio_X509(pBio, NULL, NULL, NULL);
+	if (!pCert)
+	{
+		cs_log("Error: PEM_read_bio_X509() failed");
+		BIO_free(pBio);
+		return NULL;
+	}
+
+	// version
+	osi.cert_version = (int)X509_get_version(pCert) + 1;
+
+	// valid from / to
+	ASN1_TIME *not_before = X509_get_notBefore(pCert);
+	convert_ASN1TIME(not_before, osi.cert_valid_from, sizeof(osi.cert_valid_from));
+	ASN1_TIME *not_after = X509_get_notAfter(pCert);
+	convert_ASN1TIME(not_after, osi.cert_valid_to, sizeof(osi.cert_valid_to));
+
+	// expiration
+	osi.cert_is_expired = !(X509_cmp_current_time(not_before) < 0 &&
+							X509_cmp_current_time(not_after) > 0);
+
+	// serial number
+	ASN1_INTEGER *serial = X509_get_serialNumber(pCert);
+	BIGNUM *bn = ASN1_INTEGER_to_BN(serial, NULL);
+	char *hex = BN_bn2hex(bn);
+	osi.cert_serial = NULL;
+	if (cs_malloc(&osi.cert_serial, cs_strlen(hex) + 1))
+		cs_strncpy(osi.cert_serial, strtolower(hex), cs_strlen(hex) + 1);
+	OPENSSL_free(hex);
+	BN_free(bn);
+
+	// fingerprint (SHA1)
+	unsigned char buf[SHA_DIGEST_LENGTH];
+	const EVP_MD *digest = EVP_sha1();
+	unsigned len = 0;
+	ret = X509_digest(pCert, digest, buf, &len);
+	if (ret && len == SHA_DIGEST_LENGTH)
+	{
+		char strbuf[2 * SHA_DIGEST_LENGTH + 1];
+		hex_encode(buf, strbuf, SHA_DIGEST_LENGTH);
+		osi.cert_fingerprint = NULL;
+		if (cs_malloc(&osi.cert_fingerprint, cs_strlen(strbuf) + 1))
+			cs_strncpy(osi.cert_fingerprint, strtolower(strbuf), cs_strlen(strbuf) + 1);
+	}
+
+	// subject + issuer
+	char *subj = _X509_NAME_oneline_utf8(X509_get_subject_name(pCert));
+	char *issuer = _X509_NAME_oneline_utf8(X509_get_issuer_name(pCert));
+	osi.cert_subject = NULL;
+	osi.cert_issuer = NULL;
+	if (cs_malloc(&osi.cert_subject, cs_strlen(subj) + 1))
+		cs_strncpy(osi.cert_subject, subj, cs_strlen(subj) + 1);
+	if (cs_malloc(&osi.cert_issuer, cs_strlen(issuer) + 1))
+		cs_strncpy(osi.cert_issuer, issuer, cs_strlen(issuer) + 1);
+
+	// self-signed check
+	osi.cert_is_cacert = false;
+	if (strncmp(subj, issuer, cs_strlen(issuer)) != 0)
+		osi.cert_is_cacert = true;
+	free(subj);
+	free(issuer);
+
+	// check provided certificate chain in built-in cert
+	osi.cert_is_valid_self = false;
+	X509_STORE *store_pem = X509_STORE_new();
+	if (store_pem)
+	{
+		X509 *crt = NULL;
+		while ((crt = PEM_read_bio_X509(pBio, NULL, NULL, NULL)))
 		{
-			// Load built-in cert from memory into BIO object
-			if (BIO_puts(pBio, config_cert))
+			if (!X509_STORE_add_cert(store_pem, crt))
+				cs_log("Error: X509_STORE_add_cert() failed");
+			X509_free(crt);
+		}
+
+		X509_STORE_CTX *ctx_store_pem = X509_STORE_CTX_new();
+		if (ctx_store_pem)
+		{
+			if (X509_STORE_CTX_init(ctx_store_pem, store_pem, pCert, NULL))
 			{
-				// Read cert in PEM format from BIO
-				if (PEM_read_bio_X509(pBio, &pCert, NULL, NULL))
+				ret = X509_verify_cert(ctx_store_pem);
+				if (ret >= 0)
+					osi.cert_is_valid_self = ret;
+			}
+			X509_STORE_CTX_free(ctx_store_pem);
+		}
+		X509_STORE_free(store_pem);
+	}
+
+	// check system ca-certificates.crt
+	osi.cert_is_valid_system = false;
+	osi.system_ca_file = NULL;
+	if (!osi.cert_is_valid_self)
+	{
+		X509_STORE *store_system = X509_STORE_new();
+		if (store_system)
+		{
+			const char *ca_path;
+			snprintf(system_ca_file, sizeof(system_ca_file), "%s/%s", CA_SYSTEM_LOCATION, CA_FILE_NAME);
+			if (!file_exists(system_ca_file))
+			{
+				if (!(ca_path = getenv(X509_get_default_cert_dir_env())))
+					ca_path = X509_get_default_cert_dir();
+				snprintf(system_ca_file, sizeof(system_ca_file), "%s/%s", ca_path, CA_FILE_NAME);
+			}
+
+			if (cs_malloc(&osi.system_ca_file, cs_strlen(system_ca_file) + 1))
+				cs_strncpy(osi.system_ca_file, system_ca_file, cs_strlen(system_ca_file) + 1);
+
+			if (X509_STORE_load_locations(store_system, system_ca_file, NULL))
+			{
+				X509_STORE_CTX *ctx_store_system = X509_STORE_CTX_new();
+				if (ctx_store_system)
 				{
-					// Get some cert attributes
-					// version
-					osi.cert_version = ((int) X509_get_version(pCert)) + 1;
-
-					// valid from to dates
-					ASN1_TIME *not_before = X509_get_notBefore(pCert);
-					convert_ASN1TIME(not_before, osi.cert_valid_from, sizeof(osi.cert_valid_from));
-					ASN1_TIME *not_after = X509_get_notAfter(pCert);
-					convert_ASN1TIME(not_after, osi.cert_valid_to, sizeof(osi.cert_valid_to));
-
-					// expiration
-					osi.cert_is_expired = !(X509_cmp_current_time(not_before) < 0 &&
-											X509_cmp_current_time(not_after)  > 0);
-
-					//serialNumber
-					ASN1_INTEGER *serial = X509_get_serialNumber(pCert);
-					BIGNUM *bn = ASN1_INTEGER_to_BN(serial, NULL);
-					char *hex = BN_bn2hex(bn);
-					osi.cert_serial = NULL;
-					if (cs_malloc(&osi.cert_serial, cs_strlen(hex) + 1))
+					if (X509_STORE_CTX_init(ctx_store_system, store_system, pCert, NULL))
 					{
-						cs_strncpy(osi.cert_serial, strtolower(hex), cs_strlen(hex) + 1);
+						ret = X509_verify_cert(ctx_store_system);
+						if (ret >= 0)
+							osi.cert_is_valid_system = ret;
 					}
-					OPENSSL_free(hex);
-					BN_free(bn);
-
-					// fingerprint
-					unsigned char buf[SHA_DIGEST_LENGTH];
-					const EVP_MD *digest = EVP_sha1();
-					unsigned len;
-					ret = X509_digest(pCert, digest, buf, &len);
-					if (ret && len == SHA_DIGEST_LENGTH)
-					{
-						char strbuf[2 * SHA_DIGEST_LENGTH + 1];
-						hex_encode(buf, strbuf, SHA_DIGEST_LENGTH);
-						osi.cert_fingerprint = NULL;
-						if (cs_malloc(&osi.cert_fingerprint, cs_strlen(strbuf) + 1))
-						{
-							cs_strncpy(osi.cert_fingerprint, strtolower(strbuf), cs_strlen(strbuf) + 1);
-						}
-					}
-
-					// subject + issuer
-					char *subj = _X509_NAME_oneline_utf8(X509_get_subject_name(pCert));
-					char *issuer = _X509_NAME_oneline_utf8(X509_get_issuer_name(pCert));
-					osi.cert_subject = NULL;
-					osi.cert_issuer = NULL;
-					if (cs_malloc(&osi.cert_subject, cs_strlen(subj) + 1))
-					{
-						cs_strncpy(osi.cert_subject, subj, cs_strlen(subj) + 1);
-					}
-					if (cs_malloc(&osi.cert_issuer, cs_strlen(issuer) + 1))
-					{
-						cs_strncpy(osi.cert_issuer, issuer, cs_strlen(issuer) + 1);
-					}
-
-					// self signed check
-					osi.cert_is_cacert = false;
-					if (!(strncmp(subj, issuer, cs_strlen(issuer)) == 0))
-					{
-						osi.cert_is_cacert = true;
-					}
-					OPENSSL_free(subj);
-					OPENSSL_free(issuer);
-
-					// is valid checking provided certificate chain in built-in cert
-					osi.cert_is_valid_self = false;
-					X509_STORE *store_pem = X509_STORE_new();
-					if (store_pem)
-					{
-						X509 *crt = X509_new();
-						if (crt)
-						{
-							while ((crt = PEM_read_bio_X509(pBio, NULL, NULL, NULL)))
-							{
-								if (!X509_STORE_add_cert(store_pem, crt))
-								{
-									cs_log("Error: X509_STORE_add_cert() failed");
-								}
-							}
-							X509_free(crt);
-
-							X509_STORE_CTX *ctx_store_pem = X509_STORE_CTX_new();
-							if (ctx_store_pem)
-							{
-								if (X509_STORE_CTX_init(ctx_store_pem, store_pem, pCert, NULL))
-								{
-									if ((ret = X509_verify_cert(ctx_store_pem)) >= 0)
-									{
-										osi.cert_is_valid_self = ret;
-									}
-									X509_STORE_CTX_free(ctx_store_pem);
-								}
-								X509_STORE_free(store_pem);
-							}
-						}
-					}
-
-					// is valid checking certificate chain in system ca-certificates.crt
-					osi.cert_is_valid_system = false;
-					osi.system_ca_file = NULL;
-					if (!osi.cert_is_valid_self)
-					{
-						X509_STORE *store_system = X509_STORE_new();
-						if (store_system)
-						{
-							const char *ca_path;
-							snprintf(system_ca_file, sizeof(system_ca_file), "%s/%s", CA_SYSTEM_LOCATION, CA_FILE_NAME);
-							if(!file_exists(system_ca_file))
-							{
-								if (!(ca_path = getenv(X509_get_default_cert_dir_env())))
-								{
-									ca_path = X509_get_default_cert_dir();
-								}
-								snprintf(system_ca_file, sizeof(system_ca_file), "%s/%s", ca_path, CA_FILE_NAME);
-							}
-
-							if (cs_malloc(&osi.system_ca_file, cs_strlen(system_ca_file) + 1))
-							{
-								cs_strncpy(osi.system_ca_file, system_ca_file, cs_strlen(system_ca_file) + 1);
-							}
-							if (X509_STORE_load_locations(store_system, system_ca_file, NULL))
-							{
-								X509_STORE_CTX *ctx_store_system = X509_STORE_CTX_new();
-								if (ctx_store_system)
-								{
-									if (X509_STORE_CTX_init(ctx_store_system, store_system, pCert, NULL))
-									{
-										if ((ret = X509_verify_cert(ctx_store_system)) >= 0)
-										{
-											osi.cert_is_valid_system = ret;
-										}
-										X509_STORE_CTX_free(ctx_store_system);
-									}
-									X509_STORE_free(store_system);
-								}
-							}
-							else
-							{
-								cs_log("Error: X509_STORE_load_locations() failed. Unable to load ca certificates from %s to store", system_ca_file);
-							}
-						}
-					}
-
-					// public key type
-					pKey = X509_get_pubkey(pCert);
-					osi.pkey_type = NULL;
-					if (pKey)
-					{
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L // changed in OpenSSL 1.1.0+
-						switch (EVP_PKEY_id(pKey))
-						{
-#else
-						switch (EVP_PKEY_type(pKey->type))
-						{
-#endif
-							case EVP_PKEY_RSA:
-								snprintf(ptype, sizeof(ptype), "%d bit RSA Key", EVP_PKEY_bits(pKey));
-								break;
-							case EVP_PKEY_DSA:
-								snprintf(ptype, sizeof(ptype), "%d bit DSA Key", EVP_PKEY_bits(pKey));
-								break;
-							case EVP_PKEY_EC:
-								snprintf(ptype, sizeof(ptype), "%d bit ECDSA Key", EVP_PKEY_bits(pKey));
-								break;
-							default:
-								snprintf(ptype, sizeof(ptype), "%d bit non-RSA/DSA Key", EVP_PKEY_bits(pKey));
-								break;
-						}
-						if (cs_malloc(&osi.pkey_type, cs_strlen(ptype) + 1))
-						{
-							cs_strncpy(osi.pkey_type, ptype, cs_strlen(ptype) + 1);
-						}
-					}
-					else
-					{
-						cs_log("Error: X509_get_pubkey() failed");
-					}
-				}
-				else
-				{
-					cs_log("Error: PEM_read_bio_X509() failed");
+					X509_STORE_CTX_free(ctx_store_system);
 				}
 			}
 			else
 			{
-				cs_log("Error: BIO_puts() failed");
+				cs_log("Error: X509_STORE_load_locations() failed. Unable to load CA certs from %s", system_ca_file);
 			}
+			X509_STORE_free(store_system);
 		}
-		else
+	}
+
+	// extract public key
+	pKey = X509_get_pubkey(pCert);
+	osi.pkey_type = NULL;
+	if (pKey)
+	{
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L  // changed in OpenSSL 1.1.0+
+		switch (EVP_PKEY_id(pKey))
+#else
+		switch (EVP_PKEY_type(pKey->type))
+#endif
 		{
-			cs_log("Error: BIO_new() failed");
+			case EVP_PKEY_RSA:
+				snprintf(ptype, sizeof(ptype), "%d bit RSA Key", EVP_PKEY_bits(pKey));
+				break;
+			case EVP_PKEY_DSA:
+				snprintf(ptype, sizeof(ptype), "%d bit DSA Key", EVP_PKEY_bits(pKey));
+				break;
+			case EVP_PKEY_EC:
+				snprintf(ptype, sizeof(ptype), "%d bit ECDSA Key", EVP_PKEY_bits(pKey));
+				break;
+			default:
+				snprintf(ptype, sizeof(ptype), "%d bit non-RSA/DSA Key", EVP_PKEY_bits(pKey));
+				break;
 		}
+		if (cs_malloc(&osi.pkey_type, cs_strlen(ptype) + 1))
+			cs_strncpy(osi.pkey_type, ptype, cs_strlen(ptype) + 1);
 	}
 	else
 	{
-		cs_log("Error: X509_new() failed");
+		cs_log("Error: X509_get_pubkey() failed");
 	}
 
 	BIO_free(pBio);
 	X509_free(pCert);
-
 	return pKey;
 }
 
@@ -487,7 +461,7 @@ static bool verifyBin(const char *binfile, EVP_PKEY *pubkey)
 		{
 			cs_strncpy(osi.hash_sha256, strtolower(shaVal), osi.hash_size + 1);
 		}
-		free(hash.data);
+		OPENSSL_free(hash.data);
 
 		if (pubkey && sign.data)
 		{
@@ -499,25 +473,40 @@ static bool verifyBin(const char *binfile, EVP_PKEY *pubkey)
 #endif
 			if (!mctx)
 			{
-				cs_log("Error: EVP_MD_CTX_create() failed");
-			}
-
-			// Init SHA256 verification
-			else if (!EVP_VerifyInit(mctx, EVP_sha256()))
-			{
-				cs_log("Error: EVP_VerifyInit() failed");
-			}
-
-			// Update verification with hash_sha256
-			else if (!EVP_VerifyUpdate(mctx, shaVal, cs_strlen(shaVal)))
-			{
-				cs_log("Error: EVP_VerifyUpdate() failed");
+				cs_log("Error: EVP_MD_CTX allocation failed");
 			}
 			else
 			{
-				// Finalize verification hash_sha256 against signature and public key
-				bResult = EVP_VerifyFinal(mctx, sign.data, sign.size, pubkey);
-				osi.is_verified = (bResult == 1);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L  // changed in OpenSSL 1.1.0+
+				EVP_PKEY_CTX *pkctx = NULL;
+				if (EVP_DigestVerifyInit(mctx, &pkctx, EVP_sha256(), NULL, pubkey) <= 0)
+				{
+					cs_log("Error: EVP_DigestVerifyInit() failed");
+				}
+				else if (EVP_DigestVerifyUpdate(mctx, shaVal, cs_strlen(shaVal)) <= 0)
+				{
+					cs_log("Error: EVP_DigestVerifyUpdate() failed");
+				}
+				else
+				{
+					bResult = (EVP_DigestVerifyFinal(mctx, sign.data, sign.size) == 1);
+					osi.is_verified = (bResult == 1);
+				}
+#else
+				if (!EVP_VerifyInit(mctx, EVP_sha256()))
+				{
+					cs_log("Error: EVP_VerifyInit() failed");
+				}
+				else if (!EVP_VerifyUpdate(mctx, shaVal, cs_strlen(shaVal)))
+				{
+					cs_log("Error: EVP_VerifyUpdate() failed");
+				}
+				else
+				{
+					bResult = EVP_VerifyFinal(mctx, sign.data, sign.size, pubkey);
+					osi.is_verified = (bResult == 1);
+				}
+#endif
 			}
 		}
 	}
