d3897ce7ef893b95f6a25da194db8c0c415ae061
WXbet <4-WXbet@users.noreply.git.streamboard.tv>
2025-11-08 19:25:03 +0100

    Step 16: move oscam-aes to openssl-crypto shim
    * remove USE_LIBCRYPTO, WITH_LIBCRYPTO
    * update build and configure system for fine grained library build

--- reader-nagra.c
+++ reader-nagra.c
@@ -244,9 +244,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader *reader)
 	}
 
 	BN_CTX *ctx = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx);
-#endif
 	BIGNUM *bnN = BN_CTX_get(ctx);
 	BIGNUM *bnE = BN_CTX_get(ctx);
 	BIGNUM *bnCT = BN_CTX_get(ctx);
@@ -285,9 +283,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader *reader)
 
 	ReverseMem(cta_res + 2, 88);
 	BN_CTX *ctx1 = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx1);
-#endif
 	BIGNUM *bnN1 = BN_CTX_get(ctx1);
 	BIGNUM *bnE1 = BN_CTX_get(ctx1);
 	BIGNUM *bnCT1 = BN_CTX_get(ctx1);
@@ -323,9 +319,7 @@ static int32_t NegotiateSessionKey_Tiger(struct s_reader *reader)
 
 
 	BN_CTX *ctx3 = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx3);
-#endif
 	BIGNUM *bnN3 = BN_CTX_get(ctx3);
 	BIGNUM *bnE3 = BN_CTX_get(ctx3);
 	BIGNUM *bnCT3 = BN_CTX_get(ctx3);
@@ -456,9 +450,7 @@ static int32_t NegotiateSessionKey(struct s_reader *reader)
 	ReverseMem(cta_res + 2, 64);
 	uint8_t vFixed[] = {0, 1, 2, 3};
 	BN_CTX *ctx = BN_CTX_new();
-#ifdef WITH_LIBCRYPTO
 	BN_CTX_start(ctx);
-#endif
 	BIGNUM *bnN = BN_CTX_get(ctx);
 	BIGNUM *bnE = BN_CTX_get(ctx);
 	BIGNUM *bnCT = BN_CTX_get(ctx);
