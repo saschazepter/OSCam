0fd204fd7ec7728bc47520e560cdb8290007933a
tem_invictus <teminvictus@posteo.de>
2025-11-15 12:04:05 +0100

    Resolve "Loadbalancer - fallback adjustment"
    * Allow lb_nfb_readers to be set to zero (previously minimum was 1). This means you can configure the system to have no fallback readers if desired.
    * If only one reader matches and double_check is enabled (and the CAID matches), the patch forces a fallback check by setting force_reopen = 1.
    
    thx to @lpm11

--- globals.h
+++ globals.h
@@ -372,7 +372,7 @@
 #define WIKI_URL				"https://git.streamboard.tv/common/oscam/-/wikis"
 #define BOARD_URL				"https://board.streamboard.tv"
 #ifndef CS_VERSION
-#define CS_VERSION				"2.25.11-11904"
+#define CS_VERSION				"2.25.11-11905"
 #endif
 #ifndef CS_GIT_COMMIT
 #define CS_GIT_COMMIT			"a2b4c6d8"
