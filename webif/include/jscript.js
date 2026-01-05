f9721522b3585bbe38258d5339c7dbb286c75deb
WXbet <4-WXbet@users.noreply.git.streamboard.tv>
2026-01-05 22:28:50 +0100

    Part 2: webif: wiki integration - AJAX API and Frontend
    Backend (module-webif.c):
    - Added /wiki.json API endpoint
    - New send_oscam_wiki() function returns JSON with help text
    - Looks up parameter help via wiki_get_help(config, param)
    
    Frontend (jscript.js):
    - Wiki popup system with click handlers on parameter labels
    - AJAX requests to /wiki.json endpoint
    - formatWikiText() converts markdown to HTML
    - Popup positioning, close on ESC/click outside
    
    Styling (css.css):
    - Wiki popup styling (header, content, scrollable)
    - Code blocks and inline code formatting
    - Loading/error/not-found states
    
    Templates (webif/wiki/):
    - wiki.json - Success response
    - wikierror.json - Missing parameters
    - wikinotfound.json - No help available
    
    Build system (config.sh):
    - WEBIF_WIKI feature flag (default: disabled)
    - Menu entry "Inline Wiki Help"
    - Dependency on WEBIF
    
    Enable with: ./config.sh --enable WEBIF_WIKI

--- webif/include/jscript.js
+++ webif/include/jscript.js
@@ -2527,3 +2527,109 @@ function decodeVideoguardEMM(text, target, addHideButton) {
 
 // Create Base64 Object
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+/*
+ * OSCam WebIf Wiki Help System
+ */
+var wikiPopupHtml = '<div id="wikiPopup" class="wiki-popup">' +
+	'<div class="wiki-popup-header">' +
+		'<span class="wiki-popup-title"></span>' +
+		'<span class="wiki-popup-close">&times;</span>' +
+	'</div>' +
+	'<div class="wiki-popup-content"></div>' +
+	'</div>';
+
+$(function() {
+	if (typeof oscamconf === 'undefined') return;
+
+	$('body').append(wikiPopupHtml);
+
+	$('.wiki-popup-close').click(function() {
+		$('#wikiPopup').fadeOut(200);
+	});
+
+	$(document).click(function(e) {
+		if (!$(e.target).closest('#wikiPopup').length &&
+		    !$(e.target).closest('form table a').length) {
+			$('#wikiPopup').fadeOut(200);
+		}
+	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			$('#wikiPopup').fadeOut(200);
+		}
+	});
+
+	$('form table a').off('click').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		var param;
+		if ($(this).data('p')) {
+			param = $(this).data('p');
+		} else {
+			param = $(this).parent().next().find('input,select,textarea').attr('name');
+		}
+
+		if (!param) return;
+
+		var $link = $(this);
+		var $popup = $('#wikiPopup');
+
+		$('.wiki-popup-title').text(param);
+		$('.wiki-popup-content').html('<div class="wiki-loading">Loading...</div>');
+
+		var offset = $link.offset();
+		var leftPos = offset.left + $link.outerWidth() + 10;
+		if (leftPos + 420 > $(window).width()) {
+			leftPos = $(window).width() - 420;
+		}
+		$popup.css({
+			top: offset.top - 10,
+			left: leftPos
+		}).fadeIn(200);
+
+		$.ajax({
+			url: 'wiki.json',
+			type: 'GET',
+			data: { config: oscamconf, param: param },
+			dataType: 'json',
+			success: function(data) {
+				if (data && data.text) {
+					var html = formatWikiText(data.text);
+					$('.wiki-popup-content').html(html);
+				} else {
+					$('.wiki-popup-content').html(
+						'<div class="wiki-nohelp">No help available for this parameter.<br><br>' +
+						'<a href="https://git.streamboard.tv/common/oscam/-/wikis/pages/configuration/oscam.' +
+						oscamconf + '#' + param + '" target="_blank">View Wiki &rarr;</a></div>'
+					);
+				}
+			},
+			error: function() {
+				$('.wiki-popup-content').html(
+					'<div class="wiki-error">Failed to load help.<br><br>' +
+					'<a href="https://git.streamboard.tv/common/oscam/-/wikis/pages/configuration/oscam.' +
+					oscamconf + '#' + param + '" target="_blank">View Wiki &rarr;</a></div>'
+				);
+			}
+		});
+	});
+});
+
+function formatWikiText(text) {
+	if (!text) return '';
+
+	text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	text = text.replace(/```(\w*)\n([\s\S]*?)```/g, '<pre class="wiki-code">$2</pre>');
+	text = text.replace(/```([\s\S]*?)```/g, '<pre class="wiki-code">$1</pre>');
+	text = text.replace(/`([^`]+)`/g, '<code>$1</code>');
+	text = text.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>');
+	text = text.replace(/^### (.+)$/gm, '<h4>$1</h4>');
+	text = text.replace(/^## (.+)$/gm, '<h3>$1</h3>');
+	text = text.replace(/\n/g, '<br>');
+	text = text.replace(/<\/pre><br>/g, '</pre>');
+
+	return '<div class="wiki-text">' + text + '</div>';
+}
