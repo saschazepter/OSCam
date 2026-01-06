fd257c613a83c36a0ddb96e53ba883cef4ca9205
mme89 <teminvictus@posteo.de>
2026-01-06 01:15:45 +0100

    feature: webif list numbering
    added numbering to Readers, Users, Services

--- module-webif.c
+++ module-webif.c
@@ -2000,6 +2000,7 @@ static char *send_oscam_reader(struct templatevars *vars, struct uriparams *para
 	int32_t disabled_readers = 0;
 	int32_t active_readers = 0;
 	int32_t connected_readers = 0;
+	int32_t reader_index = 1;
 
 	ll_iter_reset(&itr); //going to iterate all configured readers
 	while((rdr = ll_iter_next(&itr)))
@@ -2031,6 +2032,7 @@ static char *send_oscam_reader(struct templatevars *vars, struct uriparams *para
 			}
 #endif
 			total_readers += 1;
+			tpl_printf(vars, TPLADD, "READERNUM", "%d", reader_index);
 
 			// used for API and WebIf
 			tpl_addVar(vars, TPLADD, "READERNAME", xml_encode(vars, rdr->label));
@@ -2322,6 +2324,7 @@ static char *send_oscam_reader(struct templatevars *vars, struct uriparams *para
 				free(new_proto);
 			}
 #endif
+			reader_index++;
 		}
 	}
 
@@ -4612,6 +4615,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 	int32_t casc_users2 = 0;
 	int32_t n_request = 0;
 	int existing_insert = 0;
+	int32_t user_index = 1;
 
 	for(account = cfg.account; (account); account = account->next)
 	{
@@ -4632,6 +4636,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 	{
 		//clear for next client
 		total_users++;
+		tpl_printf(vars, TPLADD, "USERNUM", "%d", user_index);
 		isactive = 1;
 
 		status = "offline";
@@ -4982,6 +4987,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 			}
 			++clientcount;
 		}
+		user_index++;
 	}
 
 	tpl_printf(vars, TPLADD, "TOTAL_USERS", "%d", total_users);
@@ -6995,6 +7001,7 @@ static char *send_oscam_services(struct templatevars * vars, struct uriparams * 
 	while(sidtab != NULL)
 	{
 		tpl_addVar(vars, TPLADD, "SID", "");
+		tpl_printf(vars, TPLADD, "SERVICENUM", "%d", counter + 1);
 		if((strcmp(getParam(params, "service"), sidtab->label) == 0) && (strcmp(getParam(params, "action"), "list") == 0))
 		{
 			tpl_addVar(vars, TPLADD, "SIDCLASS", "sidlist");
@@ -7009,7 +7016,7 @@ static char *send_oscam_services(struct templatevars * vars, struct uriparams * 
 		else
 		{
 			tpl_addVar(vars, TPLADD, "SIDCLASS", "");
-			tpl_printf(vars, TPLADD, "SID", "<A HREF=\"services.html?service=%s&amp;action=list\">[%03i] Show Services</A>", urlencode(vars, sidtab->label), counter+1);
+			tpl_printf(vars, TPLADD, "SID", "<A HREF=\"services.html?service=%s&amp;action=list\">Show Services</A>", urlencode(vars, sidtab->label));
 		}
 		tpl_addVar(vars, TPLADD, "LABELENC", urlencode(vars, sidtab->label));
 		tpl_addVar(vars, TPLADD, "LABEL", xml_encode(vars, sidtab->label));
